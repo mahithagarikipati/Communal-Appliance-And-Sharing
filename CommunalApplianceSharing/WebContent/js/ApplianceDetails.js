f502dc9facfabf6f6d85728dcf0701d05e1333dd
adriannamckeown <71573922+adriannamckeown@users.noreply.github.com>
2020-11-22 21:07:26 -0500

    Update ApplianceDetails.js

--- CommunalApplianceSharing/WebContent/js/ApplianceDetails.js
+++ CommunalApplianceSharing/WebContent/js/ApplianceDetails.js
@@ -11,10 +11,9 @@ details.controller("detailsController", [ '$scope', '$http','$window','$mdDialog
          method : "POST",
          params : {
              'userName' : userName,
-             'appId' : id
+             'appId' : appId
          }
      }).then(function(response,$window) {
-        
          console.log(response.data);
          $scope.object = response.data;
          var container = document.getElementById("container");
@@ -25,10 +24,10 @@ details.controller("detailsController", [ '$scope', '$http','$window','$mdDialog
                   var input = document.createElement("textarea");
              input.maxLength ="500";
              input.rows ="5";
-              input.setAttribute('ng-model',response.data.id);
+              input.setAttribute('ng-model',response.data.appliance_id);
               input.value = "Lender's Name: " +response.data.firstName+" "+response.data.lastName+" "+"\r\n"
 			  + "Lender's Address: " +response.data.streetAddress+"\r\n" + "Phone Number: " +response.data.phoneNo+" "+"\r\n" 
-			  + "Price: " +response.data.price_per_day+"\r\n" + "Available Dates: " +response.data.available_from_dt+" - "+response.data.available_to_dt;
+			  + "Price: " +response.data.price_per_day+"\r\n" + "Available Dates: " +response.data.available_from_dt+" to "+response.data.available_to_dt;
             para.appendChild(input);
             container.appendChild(para);
 		})
